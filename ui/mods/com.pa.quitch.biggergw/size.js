e1b68a7ec79f7aa23fa01a8c44209a97eb81097e
Quitch <Quitch@users.noreply.github.com>
2018-03-02 20:57:31 +0000

    Mod setup

---
+++ ui/mods/com.pa.quitch.biggergw/size.js
@@ -0,0 +1,26 @@
+// Make new system sizes selectable on the size drop-down
+
+$('#game-size').append('<option value="5">VAST</option>' +
+    '<option value="6">GIGANTIC</option>' +
+    '<option value="7">RIDICULOUS</option>' +
+    '<option value="8">MARATHON</option>')
+
+// Set system size settings and add them to numberOfSystems
+
+requireGW([
+    'shared/gw_balance'
+],
+    function (balance) {
+        balance.numberOfSystems.push(
+            108, // Vast
+            144, // Gigantic
+            186, // Ridiculous
+            234 // Marathon
+        )
+        balance.galaxySizeDiffMod.push(
+            0.95,
+            0.9,
+            0.85,
+            0.8
+        )
+    })
