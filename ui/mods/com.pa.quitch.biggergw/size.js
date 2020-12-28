fff32d311f69fa0cbda9e1f217e8b25bf0ba8f7a
Quitch <Quitch@users.noreply.github.com>
2020-12-28 15:48:43 +0000

    Add debug code

--- ui/mods/com.pa.quitch.biggergw/size.js
+++ ui/mods/com.pa.quitch.biggergw/size.js
@@ -1,17 +1,28 @@
-$("#game-size").append(
-  '<option value="5">VAST</option>' +
-    '<option value="6">GIGANTIC</option>' +
-    '<option value="7">RIDICULOUS</option>' +
-    '<option value="8">MARATHON</option>'
-);
-locUpdateDocument();
+var biggerGalacticWar;
 
-requireGW(["shared/gw_balance"], function (balance) {
-  balance.numberOfSystems.push(
-    108, // Vast
-    144, // Gigantic
-    186, // Ridiculous
-    234 // Marathon
-  );
-  balance.galaxySizeDiffMod.push(0.95, 0.9, 0.85, 0.8);
-});
+if (!biggerGalacticWar) {
+  biggerGalacticWar = true;
+
+  try {
+    $("#game-size").append(
+      '<option value="5">VAST</option>' +
+        '<option value="6">GIGANTIC</option>' +
+        '<option value="7">RIDICULOUS</option>' +
+        '<option value="8">MARATHON</option>'
+    );
+    locUpdateDocument();
+
+    requireGW(["shared/gw_balance"], function (balance) {
+      balance.numberOfSystems.push(
+        108, // Vast
+        144, // Gigantic
+        186, // Ridiculous
+        234 // Marathon
+      );
+      balance.galaxySizeDiffMod.push(0.95, 0.9, 0.85, 0.8);
+    });
+  } catch (e) {
+    console.error(e);
+    console.error(JSON.stringify(e));
+  }
+}
