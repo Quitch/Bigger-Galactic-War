66c4d5f05b26382d6319bcd44dc0afc00a21f97d
Quitch <Quitch@users.noreply.github.com>
2026-07-24 21:08:58 +0100

    Remove unnecessary wrapper from our try...catch

--- ui/mods/com.pa.quitch.biggergw/zoom.js
+++ ui/mods/com.pa.quitch.biggergw/zoom.js
@@ -1,15 +1,9 @@
-var biggerGalacticWarLoaded;
-
-if (!biggerGalacticWarLoaded) {
-  biggerGalacticWarLoaded = true;
-
-  try {
-    _.defer(function () {
-      model.galaxy.zoom(Math.max(model.galaxy.zoom(), model.galaxy.minZoom()));
-      model.centerOnPlayer();
-    });
-  } catch (e) {
-    console.error(e);
-    console.error(JSON.stringify(e));
-  }
+try {
+  _.defer(function () {
+    model.galaxy.zoom(Math.max(model.galaxy.zoom(), model.galaxy.minZoom()));
+    model.centerOnPlayer();
+  });
+} catch (e) {
+  console.error(e);
+  console.error(JSON.stringify(e));
 }
